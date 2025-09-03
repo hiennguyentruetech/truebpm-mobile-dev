8309c832637bf72afdf75f6ae82e9d36a80f20e9
Hien Nguyen <nguyenthanhhien@truetech.com.vn>
2025-09-03 16:56:24 +0700

    Refactor DetailTravelClaimScreen constructor: Improve parameter handling and maintain consistency with super class.

--- lib/screens/travel_claim_screens/detail_travel_claim_screen.dart
+++ lib/screens/travel_claim_screens/detail_travel_claim_screen.dart
@@ -1,17 +1,24 @@
 import 'package:truebpm/models/core_detail_model.dart';
 import 'package:truebpm/screens/core_screens/detail_core_screen.dart';
 import 'package:truebpm/screens/travel_claim_screens/travel_claim_page_screen.dart';
+import 'package:truebpm/widgets/core/core_action_dialog.dart';
 
 /// Detail screen for Travel Claim (TRACLA)
 class DetailTravelClaimScreen extends DetailCoreScreen {
-  const DetailTravelClaimScreen({
+  DetailTravelClaimScreen({
     super.key,
-    required super.listItem,
-    super.initialTabCode,
-    super.fromTaskScreen = false,
-    super.taskId,
-    super.printReports,
-  });
+    required Map<String, dynamic> listItem,
+    String? initialTabCode,
+    bool fromTaskScreen = false,
+    String? taskId,
+    List<PrintReportOption>? printReports,
+  }) : super(
+          listItem: listItem,
+          initialTabCode: initialTabCode,
+          fromTaskScreen: fromTaskScreen,
+          taskId: taskId,
+          printReports: printReports ?? TravelClaimPageScreen.printReports,
+        );
 
   @override
   String get moduleCode => TravelClaimPageScreen.moduleCode;
